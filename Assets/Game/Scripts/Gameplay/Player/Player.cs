d9d6271cff5bf0a7711ba68c6a3fe6e0f01f7fd0
AymaneAbidane <76812961+AymaneAbidane@users.noreply.github.com>
2024-08-06 16:32:05 +0100

    Player Controlls Done For Pc and mobile

--- Assets/Game/Scripts/Gameplay/Player/Player.cs
+++ Assets/Game/Scripts/Gameplay/Player/Player.cs
@@ -1,18 +1,67 @@
+using Sirenix.Utilities;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+
+    PlayerControllers controls;
+
+    private float direction;
+
+    private Vector3 moveVector;
+    private const float CLAMP_VALUE = 7.5f;
+
+    private void Awake()
     {
-        
+        GetsInputsValuesFromInputSyteme();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GetsInputsValuesFromInputSyteme()
     {
-        
+        controls = new();
+        controls.Enable();
+        controls.Controlle.Move.performed += controllX =>
+        {
+            direction = controllX.ReadValue<float>();
+        };
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        controls.Disable();
+    }
+
+    private void Update()
+    {
+        GetInputs();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void GetInputs()
+    {
+        moveVector = Vector3.right * direction * speed;
+    }
+
+    private void Move()
+    {
+        transform.position += moveVector * Time.fixedDeltaTime;
+        ClampPosition();
+    }
+
+    private void ClampPosition()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -CLAMP_VALUE, CLAMP_VALUE), transform.position.y);
     }
 }
